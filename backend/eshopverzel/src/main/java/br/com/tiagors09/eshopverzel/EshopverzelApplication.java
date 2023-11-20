79f8f6d9934ff36884d45dc54eff0eed3fc959f2
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-11-19 21:21:18 -0300

    chore: 📂 Cria arquivos do projeto
    - Adiciona arquivos iniciais do projeto Flutter no front end.
    - Cria arquivos essenciais para o projeto Spring Boot no back end.

---
+++ backend/eshopverzel/src/main/java/br/com/tiagors09/eshopverzel/EshopverzelApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagors09.eshopverzel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopverzelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopverzelApplication.class, args);
+	}
+
+}
---
+++ backend/eshopverzel/src/test/java/br/com/tiagors09/eshopverzel/EshopverzelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagors09.eshopverzel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopverzelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
