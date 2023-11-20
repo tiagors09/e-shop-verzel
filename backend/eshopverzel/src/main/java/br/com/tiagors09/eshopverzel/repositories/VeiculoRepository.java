f33e729e6e46cefa90a49ac938876a176de06369
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2023-11-20 09:57:38 -0300

    feat(repository): 🗃️ Adiciona a interface VeiculoRepository para operações CRUD
    - Introduz a interface VeiculoRepository para permitir operações básicas no banco de dados.

---
+++ backend/eshopverzel/src/main/java/br/com/tiagors09/eshopverzel/repositories/VeiculoRepository.java
@@ -0,0 +1,9 @@
+package br.com.tiagors09.eshopverzel.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.tiagors09.eshopverzel.models.Veiculo;
+
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+
+}
