a1e31a10768a47380335bf64b1ea0ea1c8373e7a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-12-06 14:44:58 -0300

    ✨ Criar a classe Car

---
+++ frontend/lib/car.dart
@@ -0,0 +1,10 @@
+class Car {
+  int id;
+  String nome;
+  String marca;
+  String modelo;
+  String foto;
+  double preco;
+
+  Car(this.id, this.nome, this.marca, this.modelo, this.foto, this.preco);
+}
